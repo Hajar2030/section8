470192c189894f5461267e77d689cee9ee0c0ff4
Hajar2030 <hajar542002@gmail.com>
2021-10-20 09:52:04 +0200

    my first

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	double x, y;
+	cout << "Enter two numbers\n";
+	cin >> x >> y;
+	cout << x << " + " << y << " = " << x + y << "\n";
+	system("pause");
+	return 0;
+}
